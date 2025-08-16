297612949ad530864c2d7b3c6f543c553b796642
devoleper-samreen <samreenmalik52292@gmail.com>
2025-08-16 11:38:33 +0530

    add sign-in with next-auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,88 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "you@example.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials?.identifier },
+              { username: credentials?.identifier },
+            ],
+          });
+          if (!user) {
+            throw new Error("User not found with this email or username");
+          }
+
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before login");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials?.password,
+            user.password
+          );
+
+          if (!isPasswordCorrect) {
+            throw new Error("Incorrect password");
+          }
+
+          return user;
+        } catch (error: any) {
+          console.error("Error authorizing user:", error);
+          throw new Error("Error authorizing user", error);
+        }
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.username = user.username;
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.username = token.username;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+      }
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/sign-in",
+  },
+
+  session: {
+    strategy: "jwt",
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
