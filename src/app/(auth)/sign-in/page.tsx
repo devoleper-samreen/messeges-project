82eeb2f832bb6abf117a10f11a8297037b3c4ef6
devoleper-samreen <samreenmalik52292@gmail.com>
2025-08-19 11:26:31 +0530

    add sign-in frontend part

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,128 @@
-import React from "react";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@uidotdev/usehooks";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { signupSchema } from "@/schemas/signupSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader } from "lucide-react";
+import Link from "next/link";
+import { signinSchema } from "@/schemas/signinSchema";
+import { signIn } from "next-auth/react";
 
-function page() {
-  return <div>page</div>;
-}
+const page = () => {
+  const router = useRouter();
+
+  //zod impletation
+  const form = useForm({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const submitHandler = async (data: z.infer<typeof signinSchema>) => {
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+
+    if (result?.error) {
+      toast.error(result.error);
+    }
+
+    if (result?.url) {
+      router.replace("/dashboard");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1
+            className="text-4xl font-extrabold
+tracking-tight mb-6"
+          >
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+
+        {/* form */}
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(submitHandler)}
+            className="space-y-6"
+          >
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username/Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter Username/Email" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter Password"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="cursor-pointer">
+              Sign in
+            </Button>
+          </form>
+          <div className="text-center mt-4">
+            <p>
+              Already a member?{" "}
+              <Link
+                href="/sign-up"
+                className="mr-2 text-blue-600 hover:text-blue-800"
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
 export default page;
